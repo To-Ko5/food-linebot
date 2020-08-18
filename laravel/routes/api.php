fb4820f07f8b4588a3e5ac2bcdd4849908e0c326
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-08-18 11:22:46 +0900

    route修正

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -15,6 +15,5 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-
-Route::post('/parrot', 'LineBotController@parrot');
 });
+Route::post('/parrot', 'LineBotController@parrot');
